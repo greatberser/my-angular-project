ee05c72954ab03a914fde13edd15768e448be8f7
Maks <maks135254@gmail.com>
2024-09-06 14:57:47 +0300

    updated angular json

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
 import { InputPasswordComponent } from '../app/input-password/input-password.component';
 
-export const routes: Routes = [{ path: '', component: InputPasswordComponent }];
+export const routes: Routes = [
+  {
+    path: '../app/input-password/input-password.component',
+    component: InputPasswordComponent,
+  },
+];
